53df750df06f6c12dc59cb4fd65d8a988bee409d
Alex Simisi Nta <smsxander@gmail.com>
2021-08-11 01:10:42 +0200

    create navbar and footer components

---
+++ src/components/Layouts/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app-footer">
+    <footer class="bg-primary d-flex justify-content-center align-items-center">
+      <p class="text-white font-weight-bold font-size-lg p-3">
+        &copy; {{ new Date().getFullYear() }} Alexander Sms Inc.
+      </p>
+    </footer>
+  </div>
+</template>
